feeca58ef51d5b361b8ba73039fb40033090fca8
l1irik-rbk <kir.baranow2010@gmail.com>
2021-10-16 21:05:18 +0300

    feat: add st-stack

--- src/st-stack.js
+++ src/st-stack.js
@@ -13,18 +13,19 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
  module.exports = class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.stackArr = []
   }
 
-  pop() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  push(arg) {
+    this.stackArr.push(arg)
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    return this.stackArr[this.stackArr.length - 1]
+  }
+
+  pop() {
+    return this.stackArr.pop()
   }
 }
